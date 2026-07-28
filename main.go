23aaecf12805af8b0fc38825e0bf41e221e9e96a
dariodesalvo <dariodesalvo@gmail.com>
2026-07-28 16:35:20 -0300

    add commands

--- main.go
+++ main.go
@@ -1,27 +1,83 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"os"
 
 	"gator/internal/config"
 )
 
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	registeredCommands map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.registeredCommands[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.registeredCommands[cmd.name]
+	if !ok {
+		return fmt.Errorf("unknown command: %s", cmd.name)
+	}
+	return handler(s, cmd)
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("a username is required")
+	}
+
+	username := cmd.args[0]
+	err := s.cfg.SetUser(username)
+	if err != nil {
+		return fmt.Errorf("could not set user: %w", err)
+	}
+
+	fmt.Printf("User has been set to %s\n", username)
+	return nil
+}
+
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatalf("error al leer la configuración inicial: %v", err)
+		log.Fatalf("error reading config: %v", err)
 	}
 
-	err = cfg.SetUser("lane")
+	programState := &state{
+		cfg: &cfg,
+	}
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("not enough arguments provided")
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	cmd := command{
+		name: cmdName,
+		args: cmdArgs,
+	}
+
+	err = cmds.run(programState, cmd)
 	if err != nil {
-		log.Fatalf("error al actualizar el usuario: %v", err)
+		log.Fatal(err)
 	}
-
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error al releer la configuración: %v", err)
-	}
-
-	fmt.Printf("%+v\n", cfg)
 }
